9effd95fc135cc5d088179469d4cdd2d6a7902e8
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-04-12 23:03:43 +0530

    Modified get_distance function
    And added organism_locations as an array instead of using scanf for
    getting the organism locations

--- main.c
+++ main.c
@@ -4,10 +4,14 @@
 int main(void)
 {
   Point food_points[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
-  Point current_location;
   Point closest_location;
-  scanf("%d %d", &current_location.x, &current_location.y);
-  get_closest_food(food_points, 5, current_location, &closest_location);
-  printf("Location of organism: [%d %d],  Closest food target : [%d %d]\n", current_location.x, current_location.y, closest_location.x, closest_location.y);
+  Point current_location;
+  Point organism_location[] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
+  for (int i = 0; i < 5; i++)
+  {
+    current_location = organism_location[i];
+    get_closest_food(food_points, 5, current_location, &closest_location);
+    printf("Location of organism: [%d %d],  Closest food target : [%d %d]\n", current_location.x, current_location.y, closest_location.x, closest_location.y);
+  }
   return 0;
 }
--- point.c
+++ point.c
@@ -3,7 +3,7 @@
 
 double get_distance(Point a, Point b)
 {
-  return sqrt(((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y)));
+  return hypot(a.x - b.x, a.y - b.y);
 }
 
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
