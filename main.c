612f0db6af64dee6eee853cbfbe7ffcf61045d3f
Sravani Labala <58027100+sravani-labala@users.noreply.github.com>
2020-04-12 22:19:02 +0530

    Added food_points in main.c
    And taking the current_location from the standard input
    And displaying the given positions

--- main.c
+++ main.c
@@ -3,5 +3,11 @@
 
 int main(void)
 {
+  Point food_points[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  Point current_location;
+  Point closest_location;
+  scanf("%d %d", &current_location.x, &current_location.y);
+  printf("Location of organism: [%d %d]\n", current_location.x, current_location.y);
+  get_closest_food(food_points, 5, current_location, &closest_location);
   return 0;
 }
